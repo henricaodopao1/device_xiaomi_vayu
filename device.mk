8919a09254c6874a7d1fb13456e558579fcc26d1
henricaodopao1 <dragonsxbr@gmail.com>
2023-10-25 13:13:42 +0000

    vayu: inherit updateable apex
    Signed-off-by: henricaodopao1 <dragonsxbr@gmail.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
