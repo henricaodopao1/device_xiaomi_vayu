91ebca168df99d82b4842cef892a31a0898c3ccc
henricaodopao1 <dragonsxbr@gmail.com>
2023-10-26 13:22:20 +0000

    [DMN]

--- device.mk
+++ device.mk
@@ -19,6 +19,8 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+$(call inherit-product, packages/overlays/ThemeIcons/config.mk)
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
